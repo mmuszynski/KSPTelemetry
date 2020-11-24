97a56fc864ba924ba4cf3fc1aa1a7a2864eacc14
Mike Muszynski <mmuszynski@gmail.com>
2020-11-24 17:36:16 -0500

    lowered spm version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SocketWrapper", url: "https://bitbucket.org/mmuszynski/socketwrapper/src/master/", .branch("master"))
+        .package(url: "https://bitbucket.org/mmuszynski/socketwrapper/src/master/", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
