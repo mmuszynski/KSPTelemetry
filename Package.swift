98dfc807537444c70a0a04b7484b291023c23b55
Mike Muszynski <mmuszynski@gmail.com>
2022-12-09 12:11:17 -0500

    more strict requirements for keplerian

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "KSPTelemetry",
     
-    platforms: [.iOS(.v13), .macOS(.v10_13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,8 +16,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+<<<<<<< HEAD
         .package(url: "https://github.com/mmuszynski/Keplerian.git", branch: "master")
 
+=======
+        .package(url: "https://github.com/mmuszynski/Keplerian.git", .upToNextMajor(from: "0.0.0"))
+>>>>>>> more strict requirements for keplerian
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
