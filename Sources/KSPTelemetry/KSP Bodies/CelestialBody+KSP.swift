2db683351d9cc38c0afb69c0600dcdac8bea8dcb
Mike Muszynski <mmuszynski@gmail.com>
2022-12-13 23:28:24 -0500

    corrected minmus

--- Sources/KSPTelemetry/KSP Bodies/CelestialBody+KSP.swift
+++ Sources/KSPTelemetry/KSP Bodies/CelestialBody+KSP.swift
@@ -15,7 +15,7 @@ extension CelestialBody {
     public static let eve = CelestialBody(gravitationalParameter: 8.1717302e12, radius: 700000, mass: 1.2243980e23, orbit: .eve)
     public static let kerbin = CelestialBody(gravitationalParameter: 3.5316000e12, radius: 6e5, mass: 5.2915158e22, orbit: .kerbin)
     public static let mun = CelestialBody(gravitationalParameter: 6.5138398e10, radius: 2e5, mass: 9.7599066e20, orbit: .mun)
-    public static let minmus = CelestialBody(gravitationalParameter: 1.7658000e9, radius: 6e5, mass: 2.6457580e19, orbit: .minmus)
+    public static let minmus = CelestialBody(gravitationalParameter: 1.7658000e9, radius: 6e4, mass: 2.6457580e19, orbit: .minmus)
     public static let duna = CelestialBody(gravitationalParameter: 3.0136321e11, radius: 320000, mass: 4.5154270e21, orbit: .duna)
     public static let jool = CelestialBody(gravitationalParameter: 2.8252800e14, radius: 6000000, mass: 4.2332127e24, orbit: .jool)
     public static let dres = CelestialBody(gravitationalParameter: 2.1484489e10, radius: 138000, mass: 3.2190937e20, orbit: .dres)
